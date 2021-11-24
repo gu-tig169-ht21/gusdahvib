47814e58e653f9b10f0f97b218c7e16c64b6ed69
Viktor Dahlberg <vicke_dahlberg@hotmail.com>
2021-11-24 11:32:01 +0100

    initial commit

---
+++ lib/api.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import './state.dart';
+import 'package:http/http.dart' as http;
+
+const API_KEY = '0d8d9ad3-3880-4b3a-a63a-9b14338d18f6';
+const API_URL = 'https://todoapp-api-pyq5q.ondigitalocean.app';
+
+class InternetFetcher {
+
+
+
+static Future<List<Player>> addPlayer(Player player) async {
+   
+    Map<String, dynamic> json = Player.toJson(player);
+    var bodyString = jsonEncode(json);
+    var response = await http.post(
+      Uri.parse('$API_URL/todos?key=$API_KEY'),
+      body: bodyString,
+      headers: {'Content-Type': 'application/json'},
+    );
+    bodyString = response.body;
+    var list = jsonDecode(bodyString);
+
+    return list.map<Player>((data) {
+      return Player.fromJson(data);
+    }).toList();
+  }
+
+  static Future deletePlayer(String playerId) async {
+    var response = await http.delete(
+        Uri.parse('$API_URL/todos/$playerId?key=$API_KEY&_confirm=true'));
+    var bodyString = response.body;
+    var list = jsonDecode(bodyString);
+
+    return list.map<Player>((data) {
+      return Player.fromJson(data);
+    }).toList();
+  }
+
+  static Future<List<Player>> getPlayer() async {
+    var response = await http.get(Uri.parse('$API_URL/todos?key=$API_KEY'));
+    String bodyString = response.body;
+    print(bodyString);
+    var json = jsonDecode(bodyString);
+
+    return json.map<Player>((data) {
+      return Player.fromJson(data);
+    }).toList();
+  }
+
+  static Future putPlayer(Player player, value) async {
+    String playerId = player.id;
+    player.isChecked = value;
+    Map<String, dynamic> json = Player.toJson(player);
+    var bodyString = jsonEncode(json);
+    var response = await http.put(
+      Uri.parse('$API_URL/todos/$playerId?key=$API_KEY'),
+      body: bodyString,
+      headers: {'Content-Type': 'application/json'},
+    );
+  
+    }
+}
--- lib/state.dart
+++ lib/state.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './api.dart';
+import 'dart:convert';
 
 class Player extends ChangeNotifier {
   String message;
   bool isChecked;
+  String id;
 
-  Player({required this.message, this.isChecked = false});
+  Player({this.id = '', required this.message, this.isChecked = false});
+
+  static Map<String, dynamic> toJson(Player item){
+    return {
+      'title': item.message,
+      'done': item.isChecked,
+
+    };
+
 
 }
 
+static Player fromJson(Map<String, dynamic> json){
+  return Player(
+   id: json['id'],
+   message: json['title'],
+   isChecked: json['done'],
+    );
+  }
+}
+
 class MyState extends ChangeNotifier {
-  
+
+
   List<Player> _list = [];
   int _filterBy = 0;
 
   List<Player> get list => _list;
 
   int get filterBy => _filterBy;
-
-
-  void addText(Player player) {
-    list.add(player);
+  
+  Future getLista() async {
+    List<Player> list = await InternetFetcher.getPlayer();
+    _list = list;
     notifyListeners();
   }
 
-  void removeText(Player player) {
-    list.remove(player);
+  void addText(Player player) async {
+    
+    _list = await InternetFetcher.addPlayer(player);
     notifyListeners();
   }
 
-  void whenChanged(Player message, value) {
-    message.isChecked = value;
+  void removeText(Player player) async {
+   _list = await InternetFetcher.deletePlayer(player.id);
+    notifyListeners();
+  }
+
+   void whenChanged(Player message, value) async{
+    await InternetFetcher.putPlayer(message, value); 
     notifyListeners();
   }
 
@@ -39,5 +67,7 @@ class MyState extends ChangeNotifier {
     
     notifyListeners();
    }
+
+   
 
 }
